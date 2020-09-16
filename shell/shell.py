5615e35b1a7cd44346a8d9a2e8e9b7aa55c604ce
Lester Ibarra <llibarra2@miners.utep.edu>
2020-09-15 20:44:46 -0600

    milestone

--- shell/shell.py
+++ shell/shell.py
@@ -2,5 +2,18 @@
 
 import os, sys, re
 
+# save parent pid
+pid = os.getpid() 
+
+def get_input(): 
+    return os.read(0, 1024).decode()[:-1]
+
+def exec_path(args): 
+    try: 
+        os.execve(args[0], args, os.environ)
+    except FileNotFoundError: 
+        pass
+    
 if __name__ == '__main__':
-   
+    while True:
+        
